c4667f46ab270c9a7761c10d82a6d66bd60341a2
github username <github email address>
2019-04-03 12:39:40 -0700

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function fetchBooks() {
+  fetch("https://anapioficeandfire.com/api/books")
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
 }
 
 function renderBooks(json) {
@@ -12,4 +15,5 @@ function renderBooks(json) {
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
+
 })
