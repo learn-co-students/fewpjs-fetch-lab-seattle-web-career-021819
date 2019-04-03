94a095b59347136506e7185d95d4731a97966f55
Kimberly Heidorn <heidorkn@gmail.com>
2019-04-03 14:01:39 -0700

    simple fetch request

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 function fetchBooks() {
-}
+  fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(data => renderBooks(data));
+};
 
-function renderBooks(json) {
+function renderBooks(data) {
   const main = document.querySelector('main')
-  json.forEach(book => {
+  data.forEach(book => {
     const h2 = document.createElement('h2')
     h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
-  })
-}
+  });
+};
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
